be7775e0772b04d9ce6590af01fed93fd1b66da8
Nelson Moreno <thegreyabove@gmail.com>
2022-04-06 10:34:06 -0400

    initial commit

---
+++ romanNumerals.js
@@ -0,0 +1,22 @@
+function convertToRoman(num) {
+    var roman = {
+      M: 1000,
+      D: 500,
+      C: 100,
+      L: 50,
+      X: 10,
+      V: 5,
+      I: 1
+    };
+    var str = '';
+  
+    for (var i of Object.keys(roman)) {
+      var q = Math.floor(num / roman[i]);
+      num -= q * roman[i];
+      str += i.repeat(q);
+    }
+  
+    return str;
+  }
+
+  console.log(convertToRoman(99));
